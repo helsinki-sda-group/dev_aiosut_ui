2dab6b77cda4c9e4654d205ff5a4d1b4b4cf3770
Sara Pyykölä <sara.pyykola@gmail.com>
2025-05-09 17:20:52 +0300

    refactor aggregation script v1

--- tool/aggregation.py
+++ tool/aggregation.py
@@ -4,109 +4,75 @@ import pandas as pd
 import numpy as np
 import sumolib
 import xml.etree.ElementTree as et
-from difflib import SequenceMatcher as seq
 from functools import reduce
-from shapely.geometry import Polygon, LineString
 import sys
+import pyproj
 
-# %%
-output = pd.read_csv("./kamppi/simulation_output/emissions_1.csv", sep=";")
-net = sumolib.net.readNet("./kamppi/kamppi.net.xml")
-teleports = pd.read_csv("./kamppi/simulation_output/teleports_1.csv")
-# %%
-edges = []
-lanes = []
-names = []
-lons = []
-lats = []
-for edge in net.getEdges(withInternal=False):
-    edge_id = edge.getID()
-    lane_objs = edge.getLane()
-    print(lane_objs)
-    name = edge.getName()
-    points = edge.getShape()
-    if len(points) == 2:
-        shape = LineString(points)
-    else:
-        shape = Polygon(points)
-    lon, lat = net.convertXY2LonLat(shape.centroid.x, shape.centroid.y)
-    for lane_obj in lane_objs:
-        lane = edge.getLane(lane_obj)
-        print(lane)
-        edges.append(edge_id)
-        lanes.append(lane)
-        names.append(name)
-        lons.append(lon)
-        lats.append(lat)
-edge_lanes = pd.DataFrame(data={"Edge": edges, "Lane": lanes, "Name": names})
-# %%
-# lane_to_edges = {}
-# unique_lanes = list(output["vehicle_lane"].unique())
-# unique_edges = list(lanes["vehicle_lane"].values)
-# for lane in unique_lanes:
-#     edge_ratios = []
-#     for edge in unique_edges:
-#         edge_ratios.append(seq(a=lane, b=edge).ratio())
-#     lane_to_edges[lane] = unique_edges[np.argmax(edge_ratios)]
-# output.replace({"vehicle_lane": lane_to_edges}, inplace=True)
-output = pd.merge(output, edge_lanes, right_on="Lane", left_on="vehicle_lane")
-# %%
-bins = np.arange(0, 3660, 60, dtype=int)
-labels = np.arange(0, 60, dtype=int)
-output["timestep"] = pd.cut(output["timestep_time"], bins=bins, labels=labels)
-output["amount"] = np.ones(len(output), dtype=int)
-
-
-# %%
 # Helper function to calculate many vehicles' noise from a singular vehicle's noise
 def noise_reduce(series):
     return reduce(
         lambda x, y: 10 * np.log10(np.power(10, x / 10) + np.power(10, y / 10)), series
     )
 
-
-output_lanes = output["vehicle_lane"].unique()
+output = pd.read_csv("./kamppi/simulation_output/emissions_1.csv", sep=";")
+net = sumolib.net.readNet("./kamppi/kamppi.net.xml")
+teleports = pd.read_csv("./kamppi/simulation_output/teleports_1.csv")
+# %%
+edges = []
+names = []
+lons = []
+lats = []
+for edge in net.getEdges(withInternal=False):
+    edge_id = edge.getID()
+    name = edge.getName()
+    points = edge.getShape()
+    center = points[len(points)//2]
+    lon, lat = net.convertXY2LonLat(center[0], center[1])
+    edges.append(edge_id)
+    names.append(name)
+    lons.append(lon)
+    lats.append(lat)
+edge_df = pd.DataFrame(data={"Edge": edges, "Name": names, "Longitude": lons, "Latitude": lats})
+# %%
+lanes_to_edges = {}
+unique_edges = list(edge_df["Edge"].dropna().unique())
+unique_lanes = list(output["vehicle_lane"].dropna().unique())
+for edge in unique_edges:
+    lanes = list(filter(lambda unique_lanes: edge in unique_lanes, unique_lanes))
+    for lane in lanes:
+        lanes_to_edges[lane] = edge 
+output = output.replace(lanes_to_edges)
+output = pd.merge(output, edge_df, right_on="Edge", left_on="vehicle_lane")
+unnecessary_columns = ["vehicle_waiting", "vehicle_pos", "vehicle_angle", "vehicle_eclass", "vehicle_id", "vehicle_route", "vehicle_x", "vehicle_y"]
+output = output.drop(columns=unnecessary_columns)
+bins = np.arange(0, 3660, 60, dtype=int)
+labels = np.arange(0, 60, dtype=int)
+output["timestep"] = pd.cut(output["timestep_time"], bins=bins, labels=labels)
+output["Vehicles"] = np.ones(len(output), dtype=int)
 output = (
-    output.groupby(["timestep", "vehicle_lane", "vehicle_type"], observed=True)
+    output.groupby(["timestep", "Edge", "vehicle_type"], observed=True)
     .agg(
         {
-            "lon": "first",
-            "lat": "first",
             "vehicle_noise": noise_reduce,
             "vehicle_CO": "sum",
             "vehicle_CO2": "sum",
             "vehicle_HC": "sum",
             "vehicle_NOx": "sum",
             "vehicle_PMx": "sum",
-            "amount": "sum",
+            "Vehicles": "sum",
+            "Longitude": "first",
+            "Latitude": "first",
+            "Name": "first",
+            "vehicle_fuel": "sum",
+            "vehicle_electricity": "sum",
+            "vehicle_speed": "mean",
         }
     )
     .reset_index()
 )
-output.dropna(inplace=True)
-num_unique_output_lanes = len(output_lanes)
-full_output = pd.DataFrame({"vehicle_lane": np.repeat(sorted(output_lanes), 60 * 2)})
-lanes = output[["vehicle_lane", "lon", "lat"]].drop_duplicates()
-full_output = full_output.merge(lanes, how="left")
-full_output["timestep"] = np.tile(
-    np.repeat(np.arange(0, 60, dtype=int), 2), num_unique_output_lanes
-)
-full_output["vehicle_type"] = np.tile(
-    ["electric", "fuel"], 60 * num_unique_output_lanes
-)
-full_output = full_output.merge(
-    output, how="left", on=["timestep", "vehicle_type", "vehicle_lane", "lon", "lat"]
-)
-full_output.infer_objects()
-full_output["timestep"] = full_output["timestep"].astype(str).astype(int) + 1
-numeric_cols = full_output.select_dtypes(include=["int", "float"]).columns
-full_output[numeric_cols] = full_output[numeric_cols].fillna(0)
-full_output = full_output.rename(
-    {
-        "vehicle_lane": "Road",
-        "lon": "Longitude",
-        "lat": "Latitude",
-        "timestep": "Simulation timestep",
+output = output.dropna()
+output = output.infer_objects()
+output = output.rename(columns={"timestep": "Simulation timestep",
         "vehicle_type": "Mobility mode",
         "vehicle_noise": "Noise",
         "vehicle_CO2": "Carbon monoxide",
@@ -114,7 +80,25 @@ full_output = full_output.rename(
         "vehicle_HC": "Hydrocarbon",
         "vehicle_NOx": "Nitrogen oxides",
         "vehicle_PMx": "Coarse particles",
-        "amount": "Vehicles",
-    }
-)
-full_output.to_csv("tool/kamppi/simulation_output/clean_data.csv")
+        "vehicle_speed": "Average speed",
+        "vehicle_fuel": "Fuel",
+        "vehicle_electricity": "Electricity"})
+# TODO: fill in the unobserved lanes
+# num_unique_output_lanes = len(output_lanes)
+# full_output = pd.DataFrame({"vehicle_lane": np.repeat(sorted(output_lanes), 60 * 2)})
+# lanes = output[["vehicle_lane", "lon", "lat"]].drop_duplicates()
+# full_output = full_output.merge(lanes, how="left")
+# full_output["timestep"] = np.tile(
+    # np.repeat(np.arange(0, 60, dtype=int), 2), num_unique_output_lanes
+# )
+# full_output["vehicle_type"] = np.tile(
+    # ["electric", "fuel"], 60 * num_unique_output_lanes
+# )
+# full_output = full_output.merge(
+    # output, how="left", on=["timestep", "vehicle_type", "vehicle_lane", "lon", "lat"]
+# )
+# full_output["timestep"] = full_output["timestep"].astype(str).astype(int) + 1
+# numeric_cols = full_output.select_dtypes(include=["int", "float"]).columns
+# full_output[numeric_cols] = full_output[numeric_cols].fillna(0)
+# full_output.to_csv("tool/kamppi/simulation_output/clean_data.csv")
+# %%
