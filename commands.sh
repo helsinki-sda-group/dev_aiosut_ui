7b19d4f87d7dc8bddd5ee8c1bb313667af5e7bed
xpyxpyx <sara.pyykola@gmail.com>
2025-05-25 21:29:42 +0300

    script and file path refactoring

--- commands.sh
+++ commands.sh
@@ -3,3 +3,9 @@ python -m pip install -r "./tool/requirements.txt"
 
 # Python filepath for SUMO
 export PYTHONPATH="$SUMO_HOME/tools:$PYTHONPATH"
+
+# Run the simulation
+python "./simulation/functions.py"
+
+# Run the visualization app in browser
+python -m jupyter notebook "./tool/app.ipynb"
